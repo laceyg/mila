05183e38436c45b4de46fae1cfec3a7a81cb6596
Griffin Lacey <glacey@nvidia.com>
2020-05-21 21:47:42 -0400

    removed sudo 1

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,6 +5,6 @@ set -ex
 SCRIPT_PATH=$(dirname "$0")
 
 echo 'Install required apt packages'
-sudo apt-get update
-sudo apt-get install -y $(cat ${SCRIPT_PATH}/apt_packages)
+apt-get update
+apt-get install -y $(cat ${SCRIPT_PATH}/apt_packages)
 echo 'DONE: Install required apt packages'
